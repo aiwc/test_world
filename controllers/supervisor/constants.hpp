c6a4d1770474fd97ecaf7491c735d5ef2b89ceab
Chansol Hong <23516823+chshong@users.noreply.github.com>
2018-11-23 16:32:49 +0900

    Modify soccer field and line dimensions

--- controllers/supervisor/constants.hpp
+++ controllers/supervisor/constants.hpp
@@ -44,12 +44,12 @@ namespace constants {
   // Field Dimensions
   constexpr double FIELD_LENGTH = 7.8;
   constexpr double FIELD_WIDTH  = 4.65;
-  constexpr double GOAL_DEPTH   = 0.25;
-  constexpr double GOAL_WIDTH   = 0.6;
-  constexpr double PENALTY_AREA_DEPTH = 0.5;
-  constexpr double PENALTY_AREA_WIDTH = 1.3;
-  constexpr double GOAL_AREA_DEPTH = 0.2;
-  constexpr double GOAL_AREA_WIDTH = 0.85;
+  constexpr double GOAL_DEPTH   = 0.45;
+  constexpr double GOAL_WIDTH   = 1.0;
+  constexpr double PENALTY_AREA_DEPTH = 0.9;
+  constexpr double PENALTY_AREA_WIDTH = 1.8;
+  constexpr double GOAL_AREA_DEPTH = 0.4;
+  constexpr double GOAL_AREA_WIDTH = 1.3;
 
   // Ball Dimension
   constexpr double BALL_RADIUS = 0.03;
@@ -144,7 +144,7 @@ namespace constants {
   constexpr double      DEADLOCK_THRESHOLD = 0.4; // m/s
   constexpr double      DEADLOCK_RANGE = 1.5 * (ROBOT_SIZE[0] / sqrt(2) + BALL_RADIUS); // robots within this range near the ball are sent off
   constexpr std::size_t BACKPASS_TIME_LIMIT_MS = 3 * 1000; // ms
-  constexpr double      BACKPASS_BORDER = 0.35; // m
+  constexpr double      BACKPASS_BORDER = 0.5; // m
   constexpr std::size_t FREEKICK_TIME_LIMIT_MS = 3 * 1000; // ms
   constexpr double      FREEKICK_BORDER = 0.35; // m
   constexpr double      DEFAULT_PENALTY_RATIO = 0.1; // when a robot is sent out, it loses 0.1*max_meters
